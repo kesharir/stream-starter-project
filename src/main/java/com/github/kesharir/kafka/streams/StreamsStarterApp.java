7318c037527764146e853123d1613162ac019e06
kesharir <ritanshu.keshari@gmail.com>
2024-07-04 18:18:23 +0530

    Consuming, Transforming And Outputting stream. Printing topology

--- src/main/java/com/github/kesharir/kafka/streams/StreamsStarterApp.java
+++ src/main/java/com/github/kesharir/kafka/streams/StreamsStarterApp.java
@@ -2,8 +2,14 @@ package com.github.kesharir.kafka.streams;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Named;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class StreamsStarterApp {
@@ -14,5 +20,24 @@ public class StreamsStarterApp {
         config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        StreamsBuilder builder = new StreamsBuilder();
+        // 1 - stream from Kafka
+        KStream<String, String> wordCountInput = builder.stream("word-count-input");
+        // 2 - map values to lowercase
+        KTable<String, Long> wordCounts = wordCountInput.mapValues(value -> value.toLowerCase())
+        // 3 - flatmap values split by space
+                .flatMapValues(value -> Arrays.asList(value.split(" ")))
+        // 4 - select key to apply a key (we discard the old key)
+                .selectKey((ignoredKey, word) -> word)
+        // 5 - group by key before aggregation
+                .groupByKey()
+        // 6 - count occurences
+                .count(Named.as("Counts"));
+        wordCounts.toStream().to("word_count-output");
+        KafkaStreams streams = new KafkaStreams(builder.build(), config);
+        streams.start();
+        // printed the topology:
+        System.out.println(streams.toString());
     }
 }
